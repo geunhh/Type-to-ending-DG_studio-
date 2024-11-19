e7b8fae5f4ac12676f0c767dcfb1c61fb4e501b0
rmsgnl11 <rmsgnl11@naver.com>
2024-11-19 17:30:25 +0900

    bb

--- dgstudio_back/accounts/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from .views import KakaoLogin, KakaoCallback, profile_view
-
-urlpatterns = [
-    path('login/', KakaoLogin.as_view(), name='login'),
-    path('callback/', KakaoCallback.as_view(), name='kakao-callback'),
-    path('profile/', profile_view, name='profile'),
-]
